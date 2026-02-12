96a35b912356250527870c1872bcb073513cada5
Pragati2026 <25114cn382@glbitm.ac.in>
2026-02-12 10:28:39 +0530

    no r equal
    the program shows that if both the no r qual r not

---
+++ equal.c
@@ -0,0 +1,18 @@
+ #include<stdio.h>
+ int main()
+ {
+     int a,b;
+    printf("enter the first no.");
+     scanf("%d",&a);
+   printf("enter the second no.");
+     scanf("%d",&b);
+     if (a==b)
+     {
+         printf("the no r equal");
+     }
+     else 
+     {
+         printf("the no. r not equal");
+     }
+     return 0;
+ }
