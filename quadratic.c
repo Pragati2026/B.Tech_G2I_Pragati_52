ed4c5a43dd5864817e1802d62484c4194c16256a
Pragati2026 <25114cn382@glbitm.ac.in>
2026-02-19 11:36:17 +0530

    quadratic equation
    it will give real roots of all quadratic equation

---
+++ quadratic.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    int a,b,c;
+    float d,e;
+    printf("enter the coefficent of a quadratic equation :");
+    scanf("%d%d%d",&a,&b,&c);
+    if ((pow(b,2)-(4*a*c))>0)
+    {
+    d=(-b+sqrt(pow(b,2)-(4*a*c)))/(2*a);
+    e=(-b-sqrt(pow(b,2)-(4*a*c)))/(2*a);
+    printf("the 1st root is %f and 2nd is %f ",d,e);
+}
+    else {
+        printf("complex root exist");
+    }
+    return 0;
+
+}
